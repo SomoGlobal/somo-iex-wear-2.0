315163826e08149332bb00c07149038f886a59fa
James McGuire <james.mcguire@somoglobal.com>
2016-09-12 16:20:29 +0100

    less judder

--- Face/wear/src/main/java/com/somo/face/bezierlerpface/McFaceService.java
+++ Face/wear/src/main/java/com/somo/face/bezierlerpface/McFaceService.java
@@ -117,6 +117,7 @@ public class McFaceService extends CanvasWatchFaceService {
                     }
                 });
                 anim.start();
+                engine.invalidate();
             }
 
             /**/
